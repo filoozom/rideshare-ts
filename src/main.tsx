be577d89b5efcb72aca9a01692f10150f677e7e4
Philippe Schommers <philippe@schommers.be>
2021-12-23 11:32:51 +0100

    Replace with Preact boilerplate

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'preact'
+import { App } from './app'
 import './index.css'
-import App from './App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+render(<App />, document.getElementById('app')!)
